3b2e6d2c890f3b486f5bdd88a1bc1cd859b42ff0
Quynh To Tuan <quynh.totuan@gmail.com>
2017-04-27 13:00:14 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+ i = 0
+ new_array = []
+ while i < array.length do
+   value = yield array[i]
+   if value == true
+     new_array << array[i]
+   end
+   i += 1
+ end
+ new_array
 end
